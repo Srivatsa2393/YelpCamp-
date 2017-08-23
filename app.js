2c9702534b46c5906bddf3a363beb06fd0b88dd8
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-23 21:59:59 +0530

    Adding a database to the campgrounds

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 
 //tell express to use the bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+//Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
- var campgrounds = [
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//add a campground
+/* Campground.create(
+    {
+            name: 'Granite Hill',
+            image: 'http://www.ecocampuk.co.uk/wp-content/uploads/2011/08/Sussex-Campsite-with-Bell-Tents-7.jpeg'
+    }, function(err, campground){
+        if (err){
+            console.log('Error!!');
+        }else {
+            console.log('Newly created campground: ');
+            console.log(campground);
+        }
+    }); */
+
+
+/*  var campgrounds = [
         {
             name: 'Salmon Creek',
             image: 'https://media-cdn.tripadvisor.com/media/photo-s/01/a5/b5/a8/low-wray-campsite.jpg'
@@ -34,7 +60,7 @@ app.set('view engine', 'ejs');
             image: 'http://www.fishergroundcampsite.co.uk/data1/images/camp1.jpg'
         }
     ];
-
+ */
 
 
 //adding landing page
@@ -46,8 +72,18 @@ app.get('/', (req, res) => {
 
 //campgrounds route
 app.get('/campgrounds', (req, res) => {
-   
-    res.render('campgrounds', {campgrounds: campgrounds});
+    
+    //get all campgrounds from db
+    Campground.find({}, function(err, allCampgrounds){
+        if (err){
+            console.log('Error!!');
+        }else {
+            res.render('campgrounds', {campgrounds: allCampgrounds})
+        }
+    });
+
+
+    //res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 
@@ -57,10 +93,18 @@ app.post('/campgrounds', (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground= {name: name, image: image};
-    campgrounds.push(newCampground);
+    //Create a new campground and save it to the database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if (err){
+            console.log(err);
+        }else{
+            res.redirect('/campgrounds');
+        }
+    });
+    //campgrounds.push(newCampground);
     //redirect back to campgrounds page 
 
-    res.redirect('/campgrounds');
+    //res.redirect('/campgrounds');
 });
 
 
