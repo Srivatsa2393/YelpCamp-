4ce01d14085deae52bf67508a2fc6f587baaee85
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-30 00:20:28 +0530

    added sidebar to show page and display og comments done nicely so basically styling of the show page

--- seeds.js
+++ seeds.js
@@ -8,17 +8,17 @@ var data = [
     {
         name: 'Clouds Rest', 
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Tenting_at_Joseph_A._Citta.jpg/250px-Tenting_at_Joseph_A._Citta.jpg',
-        description: 'The camp of the century'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
     },
     {
         name: 'Organic by sleep', 
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Campsite-Oludeniz.JPG/250px-Campsite-Oludeniz.JPG',
-        description: 'Living in a organic farm'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
     },
     {
         name: 'River by the side', 
         image: 'https://media-cdn.tripadvisor.com/media/photo-s/01/a5/b5/a8/low-wray-campsite.jpg',
-        description: 'Doing camping and eating sea food'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
     }
 ];
 
