787ce5ee982d8442b5ef5d7ab9e7c9801be15e92
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-30 22:47:56 +0530

    Creating a new User model along with it done the configuration of passport

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = require('./models/user');
+
 
 mongoose.connect('mongodb://localhost/auth_demo_app');
 
 var app = express();
-
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('express-session')({
+    secret: 'Srivatsa never ever give up',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 app.get('/', (req, res) => {
